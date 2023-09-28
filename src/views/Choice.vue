2e0cb8cf33ac91474acfa7ed7a7a847eb25e5e96
unknown <shomirsaidov.abubakr@mail.ru>
2023-09-28 20:23:44 +0300

    final inshaaallah

--- src/views/Choice.vue
+++ src/views/Choice.vue
@@ -2,17 +2,26 @@
 
     <div class="flex justify-center items-center w-full h-full">
 
-        <div class="bg-white shadow-xl p-8 min-w-1/2 flex flex-col space-y-4 rounded-md" style="background-image: radial-gradient( circle 369px at -2.9% 12.9%,  rgba(247,234,163,1) 0%, rgba(236,180,238,0.56) 46.4%, rgba(163,203,247,1) 100.7% );">
-            <div class="shadow-xl p-2 rounded-xl border-2 cursor-pointer bg-white" @click="() => this.$router.push('/session')">
-                <h1 class="text-lg font-bold">Подобрать сессию Jobify</h1>
-            </div>
-            <div class="shadow-xl p-2 rounded-xl border-2 cursor-pointer bg-white" @click="() => this.$router.push('/it')">
-                <h1 class="text-lg font-bold">Подобрать курсы по программированию</h1>
-            </div>
-            <div class="shadow-xl p-2 rounded-xl border-2 cursor-pointer bg-white" @click="() => this.$router.push('/lang')">
-                <h1 class="text-lg font-bold">Подобрать курсы языкознания</h1>
+        <div>
+            <div class="p-4 rounded-xl shadow-xl">
+                <h1 style="color: rgb(255, 255, 255);" class="text-2xl font-black">Jobify Tech Test</h1>
             </div>
 
+            <div class="bg-white shadow-xl p-8 min-w-1/2 flex flex-col space-y-4 rounded-md" style="background-image: radial-gradient( circle 369px at -2.9% 12.9%,  rgba(247,234,163,1) 0%, rgba(236,180,238,0.56) 46.4%, rgba(163,203,247,1) 100.7% );">
+                <h1 class="font-black text-xl">Пройдите тест и найдите <br> новые возможности</h1>
+                <div class="shadow-xl p-2 rounded-xl border-2 cursor-pointer bg-white" @click="() => this.$router.push('/it')">
+                    <h1 class="text-lg font-bold">Подобрать курсы по программированию</h1>
+                </div>
+                <div class="shadow-xl p-2 rounded-xl border-2 cursor-pointer bg-white" @click="() => this.$router.push('/lang')">
+                    <h1 class="text-lg font-bold">Подобрать курсы языкознания</h1>
+                </div>
+                <div  class="flex justify-center p-4">
+                    <img src="../assets/qr.png" width="140" alt="">
+                </div>
+
+                <h1 class="">Doleap Team & Abubakr Shomirsaidov</h1>
+
+            </div>
         </div>
 
     </div>
--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -1,19 +1,24 @@
 <template>
 
-    <div class="flex justify-center items-center w-full h-full">
+    <div class="flex justify-center items-center w-full h-full ">
 
-    <div class="lg:w-1/4 shadow-lg border-2 rounded-xl bg-white">
+    <div class="lg:w-1/3 shadow-lg border-2 rounded-xl bg-white">
 
         <div class="p-8 rounded-xl rounded-b-none" style="background-image: radial-gradient( circle 369px at -2.9% 12.9%,  rgba(247,234,163,1) 0%, rgba(236,180,238,0.56) 46.4%, rgba(163,203,247,1) 100.7% );">
             <h1 class="font-black text-xl p-8">Наша подборка для вас</h1>
             <!-- <input type="range" v-model="this.progressValue"> -->
-            <h1 class="">Спасибо !!!</h1>
+            <div class="flex justify-center" @click="refresh">
+                <h1 class="font-semibold rounded-xl text-blue-700 p-2 cursor-pointer bg-gray-100">Начать заново</h1>
+            </div>
         </div>
 
         <div class="flex flex-col space-y-2 rounded-xl rounded-t-none bg-gray-200">
             <div class="flex items-center space-x-4 rounded-xl bg-white p-4" v-for="reco in this.stats">
-                <img :src="require('../assets/' + reco.name + '.png')" alt="" width="50">
-                <h1 class="font-black text-lg">{{ reco.name }}</h1>
+                <img :src="require('../assets/' + reco.name + '.png')" class="rounded-full" alt="" width="50">
+                <div class="flex items-center space-x-4">
+                    <h1 class="font-black text-lg text-start">{{ reco.name }}</h1>
+                    <span class="bg-green-300 text-green-900 rounded-full p-2">{{ reco.scores }}</span>
+                </div>
             </div>
         </div>
 
@@ -31,11 +36,16 @@ export default {
     data: () => ({
         stats: null
     }),
+    methods: {
+        refresh() {
+            location.href = '/'
+        }
+    },
     mounted() {
         let data = this.$store.state[this.$route.params.quiz]
         data.sort((a, b) => (a.scores < b.scores) ? 1 : -1)
         this.stats = data
-        setTimeout(() => location.href = '/',45000)
+        setTimeout(() => location.href = '/',150000)
     }
 }
 </script>
