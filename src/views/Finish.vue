82b8dd0807fc0bcf1868a1633d6a947b71f30371
unknown <shomirsaidov.abubakr@mail.ru>
2023-09-28 12:30:40 +0300

    step 2

--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -2,7 +2,7 @@
 
     <div class="flex justify-center items-center w-full h-full">
 
-    <div class=" w-1/4 shadow-lg border-2 rounded-xl bg-white">
+    <div class="lg:w-1/4 shadow-lg border-2 rounded-xl bg-white">
 
         <div class="p-8 rounded-xl rounded-b-none" style="background-image: radial-gradient( circle 369px at -2.9% 12.9%,  rgba(247,234,163,1) 0%, rgba(236,180,238,0.56) 46.4%, rgba(163,203,247,1) 100.7% );">
             <h1 class="font-black text-xl p-8">Наша подборка для вас</h1>
